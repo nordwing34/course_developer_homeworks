e2d470db8b6ae6f260841a648e847646036b5bf7
Влад Нордвинг <vlad@nordwing.ru>
2023-02-08 19:14:31 +0300

    homework 4: task 2

--- hw04/Program.cs
+++ hw04/Program.cs
@@ -7,6 +7,7 @@
 2, 4 -> 16
 */
 
+/*
 System.Console.WriteLine("Введите два числа");
 System.Console.Write("Основание степени: ");
 int numbA = int.Parse(System.Console.ReadLine());
@@ -30,12 +31,47 @@ int myPow(int par1, int par2){
 	return ret;
 }
 System.Console.WriteLine($"Результат: {myPow(numbA, numbB)}");
-
+*/
 
 
 
 // Задание 2 **********************************************************************************
 /*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
 
+// Вводим число
+System.Console.Write("Введите число: ");
+int number2 = int.Parse(System.Console.ReadLine());
+
+// Число должно быть натуральным
+if(number2 < 1){
+	System.Console.WriteLine("Число меньше 1");
+} else {
+	System.Console.WriteLine($"Сумма цифр в этом числе = {mySum(number2)}");
+}
+
+// Функция подсчитывает сумму цифр в числе
+int mySum(int param1){
+	int ret = 0;
+	int index = param1;
+
+	while(index > 0){
+		ret = ret + (index % 10);
+		index = index / 10;
+	}
+	return ret;
+}
+
+
+
+// Задание 3 **********************************************************************************
+/*
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
 */
 
