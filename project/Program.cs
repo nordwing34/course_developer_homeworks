bfc19bd7bebd707a5f7c77c3018b0af66c54e7e4
Влад Нордвинг <vlad@nordwing.ru>
2023-03-08 20:21:35 +0300

    final project: add function for print source array

---
+++ project/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Итоговый проект
+
+Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+длина которых меньше либо равноа 3 символа.
+*/
+
+string[] myStrArray = new string[] {
+	"Строка Раз",
+	"Строка Два",
+	"Строка Три",
+	"Строка Четыре"
+};
+
+System.Console.Clear();
+System.Console.WriteLine("Исходный массив строк: ");
+GetSourceArray(myStrArray);
+
+
+
+/*** Функции ******************************************************************/
+void GetSourceArray(string[] sourceArr){
+	foreach (var item in sourceArr){
+		System.Console.WriteLine($"{item}");
+	}
+}
+
